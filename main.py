a65a00dcfb588821d704cdc436f2e4dad12497d5
John Adams Jr <john.c.adams32@gmail.com>
2022-01-24 22:59:55 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+# For Day 26 I create a program that takes any string and converts it into the nato phonetic alphabet. Use this
+# project to convert your name or anything else easily, so you  can be precise when spelling to someone else.
+
+# Import files needed
+import pandas
+Nato_data_frame = pandas.DataFrame("nato_phonetic_alphabet.csv")
+
+# Loop through rows of a data frame
+# for (index, row) in student_data_frame.iterrows():
+#     #Access index and row
+#     #Access row.student or row.score
+#     pass
+
+# Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
+
+#TODO 1. Create a dictionary in this format:
+{"A": "Alfa", "B": "Bravo"}
+
+#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+
