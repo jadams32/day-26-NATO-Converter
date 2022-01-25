ccf0e0e7967c614d07e40dd9e62d6bc227a0ff2d
John Adams Jr <john.c.adams32@gmail.com>
2022-01-25 00:27:48 -0700

    Base code working, needs refactoring, and comments

--- main.py
+++ main.py
@@ -3,8 +3,8 @@
 
 # Import files needed
 import pandas
-Nato_data_frame = pandas.DataFrame("nato_phonetic_alphabet.csv")
-
+nato_data_frame = pandas.read_csv("nato_phonetic_alphabet.csv")
+print(nato_data_frame)
 # Loop through rows of a data frame
 # for (index, row) in student_data_frame.iterrows():
 #     #Access index and row
@@ -14,8 +14,13 @@ Nato_data_frame = pandas.DataFrame("nato_phonetic_alphabet.csv")
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
 
-#TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
+# TODO 1. Create a dictionary in this format:
+nato_dict = {row.letter: row.code for (index, row) in nato_data_frame.iterrows()}
+print(nato_dict)
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
+# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+user_input = input("Enter your word to convert to NATO phonetics.\n").upper()
+
+nato_output = [nato_dict[letter] for letter in user_input]
+print(nato_output)
